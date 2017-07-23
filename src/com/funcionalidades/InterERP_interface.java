5caa4d4794153bab5c571f42cf4e2794964f2219
david_000 <david_000@david-pc>
2017-07-23 10:00:39 -0300

    Inicio

---
+++ src/com/funcionalidades/InterERP_interface.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.funcionalidades;
+
+import javax.swing.tree.DefaultTreeModel;
+
+/**
+ *
+ * @author david_000
+ */
+public interface InterERP_interface {
+    abstract  public DefaultTreeModel criaMenu_Arvore();
+    abstract  public void adiona_Tab(String tab);
+}
